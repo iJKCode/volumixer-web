d21bcec53e41dd48f70d730bcff4e942e4eb731e
iJKCode <Kusters.Jens@gmail.com>
2025-03-30 19:15:35 +0200

    initial commit

---
+++ svelte.config.js
@@ -0,0 +1,21 @@
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		paths: {
+			base: "/app",
+		},
+		adapter: adapter({
+			fallback: "app.html",
+			strict: true,
+		}),
+	}
+};
+
+export default config;
